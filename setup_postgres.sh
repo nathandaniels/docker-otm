5a23bb069db1e120a7313348151b95334c45e5d2
nathandaniels <nathandaniels@mac.com>
2019-02-01 16:32:36 +1030

    Update setup_postgres.sh

--- setup_postgres.sh
+++ setup_postgres.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 service postgresql start
-postgres psql -c "CREATE USER pick_an_otm_username ENCRYPTED PASSWORD 'pick_a_password'"
-postgres psql template1 -c "CREATE EXTENSION IF NOT EXISTS hstore"
-postgres psql template1 -c "CREATE EXTENSION IF NOT EXISTS fuzzystrmatch"
-postgres psql -c "CREATE DATABASE pick_an_otm_db_name OWNER pick_an_otm_username"
-postgres psql pick_an_otm_db_name -c "CREATE EXTENSION IF NOT EXISTS postgis"
+#postgres psql -c "CREATE USER pick_an_otm_username ENCRYPTED PASSWORD 'pick_a_password'"
+#postgres psql template1 -c "CREATE EXTENSION IF NOT EXISTS hstore"
+#postgres psql template1 -c "CREATE EXTENSION IF NOT EXISTS fuzzystrmatch"
+#postgres psql -c "CREATE DATABASE pick_an_otm_db_name OWNER pick_an_otm_username"
+#postgres psql pick_an_otm_db_name -c "CREATE EXTENSION IF NOT EXISTS postgis"
 
